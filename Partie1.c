c0da4cd547caed52980c7dd681b8c0f467dfbcff
ROKATG0D <58750536+ROKATG0D@users.noreply.github.com>
2021-10-19 16:39:54 +0200

    Update Partie1.c

--- Partie1.c
+++ Partie1.c
@@ -3,3 +3,13 @@
 /* Etudiant(e) : Yassine MSILINI, Sarah BOUNDAOUI */
 /* L3 Informatique Paris-Saclay 2021-2022         */
 /**************************************************/
+
+
+/* Bibliothéque C et facilité syntaxique */
+#define ISNOT !=
+#define NOT !
+#define AND &&
+#define OR ||
+#define then
+
+type def enum {false, true} bool;
