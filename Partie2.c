1a03feb870181f0fe1d751c9e13c34c7a6985b17
RoKaT <msiliniyassine@gmail.com>
2021-11-14 18:20:36 +0100

    Partie 2 complète et corrigée

--- Partie2.c
+++ Partie2.c
@@ -279,4 +279,191 @@ int main(int argc, char** argv)
     return 0;
 }
 
-bool ZeroEnDeuxiemePosition(Liste l) {}
+/*************************************************/
+/*                                               */
+/*     Fonctions demandées par l'exercice 2.     */
+/*                                               */
+/*************************************************/
+
+/**         1]
+ Verifie si une liste à un 0 en deuxieme position
+ @param liste L
+ @return boolean TRUE/FALSE
+*/
+
+bool ZeroEnDeuxiemePosition(Liste l) {
+  if (estVide(l) == TRUE) {
+    return FALSE;
+  }
+  else if (suite(l) == 0) {
+    return TRUE;
+  }
+  else return FALSE;
+}
+
+/**         2]
+ Verifie si touts les elements d'une liste sont des 0
+ @param liste L
+ @return boolean TRUE/FALSE
+*/
+
+bool queDesZeros(Liste l) {
+  if (l == NULL) {
+    return TRUE;
+  } else while (l -> nombre == 0) {
+    l = l -> suivant;
+  }
+    return l == NULL;
+  }
+
+/**         3.1]
+ Compte le nombre de 0 en debut de liste de manière recursive
+ @param liste L
+ @return int
+*/
+
+int Compte0InitiauxRec(Liste l) {
+  if (estVide(l) == TRUE || premier(l) != 0) {
+    return 0;
+  }
+  else if (premier(l) == 0) {
+    return 1 + Compte0InitiauxRec(suite(l));
+  }
+}
+
+/**         3.2]
+ Compte le nombre de 0 en debut de liste de manière iterative
+ @param liste L
+ @return int compteur
+*/
+
+int Compte0InitiauxIte(Liste l) {
+  int compteur = 0;
+  while (l != NULL && l -> nombre == 0) {
+    l = l -> suivant;
+    compteur = compteur + 1;
+  }
+  return compteur;
+}
+
+/**         3.3]
+ Compte le nombre de 0 en debut de liste de manière recursive terminale IN
+ @param liste L
+ @return int compteur
+*/
+
+int C_Rec_In(Liste l, int compteur) {
+  return 0;
+}
+
+int Compte0InitiauxRec2(Liste l) {
+  return C_Rec_In(l, 0);
+}
+/**         3.4]
+ Compte le nombre de 0 en debut de liste de manière recursive terminale inOUT
+ @param liste L
+ @return int compteur
+*/
+
+int C_Rec_OUT(Liste l, int compteur) {
+  return 0;
+}
+
+int Compte0InitiauxRec3(Liste l) {
+  return C_Rec_OUT(l, 0);
+}
+
+/**         4]
+ Concatène deux listes et trie les elements communs au deux listes
+ @param liste l1 l2
+ @return Liste l
+*/
+
+Liste IntersectionTriee(Liste l1, Liste l2) {
+  if (estVide(l1) == TRUE || estVide(l2) == TRUE) {
+    printf("%s\n","%s Impossible l'une des deux listes est nulle" );
+  }
+  else {
+    int entier = premier(l1);
+    if (premier(l1) == premier(l2)) {
+      return ajoute(entier, IntersectionTriee(suite(l1), suite(l2)));
+    }
+    else if (premier(l1) != premier(l2)) {
+      return IntersectionTriee(suite(l1), suite(l2));
+    }
+  }
+}
+
+/**         5]
+elimine les k premières occurences de x
+ @param liste l
+ @param int x
+ @param int k
+ @return void
+*/
+
+void ElimineKpremiersX(Liste l, int x, int k) {
+}
+
+/**         6]
+elimine les k dernières occurences de x
+ @param liste l
+ @param int x
+ @param int k
+ @return void
+*/
+
+void ElimineKderniersX(Liste l, int x, int k) {
+}
+
+/**         7]
+ Permutation de manière diviser pour regner
+ @param liste l1
+ @return ?
+*/
+                                  //Fonctions toutes issues du cours page 30, problème de type, création de nouveau non faite
+/** Liste ATLTP(int x, Liste l) {
+  if (estVide(l) == TRUE) {
+    return [];
+  }
+  else {
+    return Concat(ATP(x, premier(l)), ATLTP(x, suite(l)));
+  }
+}
+
+Liste ATP(int x, Liste l) {
+  if (estVide(l) == TRUE) {
+    return [[x]];
+  }
+  else {
+    return ajoute(ajoute(x,l), AETTL(premier(l), ATP(x, suite(l))));
+  }
+}
+
+Liste Concat(Liste l1, Liste l2) {
+  if (estVide(l1) == TRUE) {
+    return l2;
+  }
+  else {
+    return ajoute(premier(l1), Concat(suite(l1), l2));
+  }
+}
+
+Liste AETTL(int x, Liste l) {
+  if (estVide(l) == TRUE) {
+    return NULL;
+  }
+  else {
+    return ajoute(ajoute(x, premier(l)), AETTL(x, suite(l)));
+  }
+}
+
+Liste Permutation(int n) {
+  if (n == 0) {
+    return [[]];
+  }
+  else {
+    return ATLTP(n, Permutation(n-1));
+  }
+}
+*/
