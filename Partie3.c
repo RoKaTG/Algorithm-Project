0dc9132b5fbf507ca64c871240896bc75896abed
RoKaT <msiliniyassine@gmail.com>
2021-12-04 18:07:23 +0100

    Commencement partie 3

---
+++ Partie3.c
@@ -0,0 +1,24 @@
+
+/**************************************************/
+/* Partie 3 du Tp en autonomie d'Algorithmique    */
+/* Etudiant(e) : Yassine MSILINI, Sarah BOUNDAOUI */
+/* L3 Informatique Paris-Saclay 2021-2022         */
+/**************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*************************************************/
+/*                                               */
+/*                sucre syntaxique               */
+/*                                               */
+/*************************************************/
+
+#define AND &&
+#define OR ||
+#define ISNOT !=
+#define NOT !
+#define then
+
+typedef enum { FALSE, TRUE} bool;
