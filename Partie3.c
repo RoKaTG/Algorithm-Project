6f5f3ef7fdc94538bb8ca5722c586ffe8fc2c7c0
RoKaT <msiliniyassine@gmail.com>
2021-12-05 14:31:47 +0100

    Création Construit noir et blanc commentées

--- Partie3.c
+++ Partie3.c
@@ -20,5 +20,46 @@
 #define ISNOT !=
 #define NOT !
 #define then
+#define NTREES 4
 
 typedef enum { FALSE, TRUE} bool;
+
+
+/*************************************************/
+/*                                               */
+/*                structure bloc_image           */
+/*                                               */
+/*************************************************/
+
+typedef struct bloc_image {
+  bool toutnoir ;
+  struct bloc_image * fils[4];
+} bloc_image;
+typedef bloc_image *image;
+
+
+/**     1.1]
+ Construit une image blanche
+ @param non
+ @return NULL
+*/
+
+image Construit_blanc() {
+  return NULL;
+}
+
+
+/**     1.2]
+ Construit une image noire
+  @param non
+  @return image res
+*/
+
+image Construit_noir() {
+  image res = (image)malloc(sizeof(bloc_image));
+  res -> toutnoir = TRUE;
+  for (int i = 0; i < NTREES; i++) {
+    res -> fils[i] = NULL;
+  }
+  return res;
+}
